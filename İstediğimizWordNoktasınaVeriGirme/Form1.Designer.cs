292d3dc91ba195b318cf89680ddb9de0a72ae307
budurmete <budurmete@gmail.com>
2022-03-02 17:39:30 +0300

    Proje dosyası ekle.

---
+++ İstediğimizWordNoktasınaVeriGirme/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace İstediğimizWordNoktasınaVeriGirme
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAdSoyad = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(55, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(344, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Adınızı Ve Soyadınızı Giriniz: ";
+            // 
+            // txtAdSoyad
+            // 
+            this.txtAdSoyad.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtAdSoyad.Location = new System.Drawing.Point(388, 85);
+            this.txtAdSoyad.Name = "txtAdSoyad";
+            this.txtAdSoyad.Size = new System.Drawing.Size(177, 30);
+            this.txtAdSoyad.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(397, 134);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 50);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Bilgiyi Worde Gönderin!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 378);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(739, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "butona Bastıktan sonra ekranınıza  \"File Created!\" yazıyorsa Dosyanızı KOD da bel" +
+    "irttiğiniz yerde oluşmuş olucaktır . ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(216, 407);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(544, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "eğer butona bastıktan sonra  \"File not Found!\" yazıyorsa kod da hatanız var demek" +
+    "ttir";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Red;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(12, 335);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(112, 29);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "DİKKAT!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtAdSoyad);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAdSoyad;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ İstediğimizWordNoktasınaVeriGirme/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+using Word = Microsoft.Office.Interop.Word;/* sağ da bulunan Çözüm gezgininde bulunan Dosya ismine sağ tıklayın 
+ve ekle/Başvurlar/COM/ Microsoft Word   ; yazın ve cıkan referasının kutucuğna tik atarak tamam tuşuna basınız*/
+
+namespace İstediğimizWordNoktasınaVeriGirme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Bul ve Değiştir Mothodu 
+        private void BulVEdegistir(Word.Application wordApp, object ToFindText, object replaceWithText)
+        {
+            object matchCase = true;
+            object matchWholeWord = true;
+            object matchWildCards = false;
+            object matchSoundLike = false;
+            object nmatchAllforms = false;
+            object forward = true;
+            object format = false;
+            object matchKashida = false;
+            object matchDiactitics = false;
+            object matchAlefHamza = false;
+            object matchControl = false;
+            object read_only = false;
+            object visible = true;
+            object replace = 2;
+            object wrap = 1;
+
+            wordApp.Selection.Find.Execute(ref ToFindText,
+                ref matchCase, ref matchWholeWord,
+                ref matchWildCards, ref matchSoundLike,
+                ref nmatchAllforms, ref forward,
+                ref wrap, ref format, ref replaceWithText,
+                ref replace, ref matchKashida,
+                ref matchDiactitics, ref matchAlefHamza,
+                ref matchControl);
+        }
+
+        // word Dosyası oluşturma Methodu
+        private void CreateWordDocument(object filename, object SaveAs)
+        {
+            Word.Application wordApp = new Word.Application();
+            object missing = Missing.Value;
+            Word.Document myWordDoc = null;
+
+            if (File.Exists((string)filename))
+            {
+                object readOnly = false;
+                object isVisible = false;
+                wordApp.Visible = false;
+
+                myWordDoc = wordApp.Documents.Open(ref filename, ref missing, ref readOnly,
+                                        ref missing, ref missing, ref missing,
+                                        ref missing, ref missing, ref missing,
+                                        ref missing, ref missing, ref missing,
+                                        ref missing, ref missing, ref missing, ref missing);
+                myWordDoc.Activate();
+
+                //Bul ve Değiştir
+
+                this.BulVEdegistir(wordApp, "<name>", txtAdSoyad.Text);
+                //this.FindAndReplace(wordApp, "<firstname>", textBox2.Text);
+                //this.FindAndReplace(wordApp, "<birthday>", dateTimePicker1.Value.ToShortDateString());
+                //this.FindAndReplace(wordApp, "<date>", DateTime.Now.ToShortDateString());
+            }
+            else
+            {
+                MessageBox.Show("File not Found!");
+            }
+
+            //  //farklı Kaydet
+            myWordDoc.SaveAs2(ref SaveAs, ref missing, ref missing, ref missing,
+                            ref missing, ref missing, ref missing,
+                            ref missing, ref missing, ref missing,
+                            ref missing, ref missing, ref missing,
+                            ref missing, ref missing, ref missing);
+
+            myWordDoc.Close();
+            wordApp.Quit();
+            MessageBox.Show("File Created!");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CreateWordDocument(@"C:\Users\BİLGİSAYAR\Desktop\DOSYA İSMİ\ Word Dosayası .doc", @"C:\Users\BİLGİSAYAR\Desktop\DOSYA İSMİ\ Word Dosayası .doc");
+
+            //BİLGİSAYAR ismi yerine kendi bilgisayınızın ismini Yazın....... Uygulamınızı oluşturduğunuz Dosyanın İSmini de "DOSYA İSMİ" kISmına yazın"
+            // Word Dosayası Yerinede Değişim Yapmak istediğiniz word dosyasının İSmini Yazın
+            //ilk "@" olduğu yere Word şablonuzu ikinci "@" yerede yeni olan word dosyanızın adını girin 
+
+            //ÖNEMLİ!!!!!
+            //ikinci "@" olduğu konuma girdiğiniz adın ve soy ismin worde aktarılmış halini nereye kaydetmek istiyorsanız oranın konumunu yazın
+        }
+    }
+}
